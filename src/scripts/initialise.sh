59eb273a52acb348c7477c7c0caacb0464d154b7
Rahul <rahulreddyr3@gmail.com>
2019-09-23 00:22:25 +0530

    [Rahul] changed init to create a separate file for grit precommit hook

--- src/scripts/initialise.sh
+++ src/scripts/initialise.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+
 if [ -d ./.git ];
 then
   mkdir .grit
@@ -13,13 +14,17 @@ else
   echo ".grit" >> .gitignore;
   echo "grit.sh" >> .gitignore;
 fi
+
 cat >> grit.sh <<EOGS
 #!/bin/bash
 #This script will be executed before each commit
 exit 0
 EOGS
+
 cd .git/hooks
-cat >> pre-commit << 'EOPCF'
+
+touch grit-pre-commit-hook
+cat >> grit-pre-commit-hook << 'EOGPCH'
 ## The below part is used by grit to run the grit script before each commit
 #!/bin/bash
 echo $pwd
@@ -27,22 +32,38 @@ if [ -d ./.grit/1 ]; then rm -rf ./.grit/1; fi
 mkdir ./.grit/1
 cp -r `ls -A | grep -v ".grit"` ./.grit/1
 cd ./.grit/1/
-git stash save --keep-index --include-untracked grit_1
+git stash save --keep-index --include-untracked --quiet grit_1
 sh grit.sh
 status=$?
-if [ $status != 0 ]; then
+if [ $status != 0 ]
+then
    	cat <<\EOF
-This commit is red. Your grit script failed. 
+This commit is red.
+Your grit script failed.
+Please check and recommit.
 EOF
 	exit 1
 else
-cat <<\EOF
-Congratulations your commit is green.
+        cat <<\EOF
+Your commit is green.
+Committing changes
 EOF
 	exit 0
 fi
-EOPCF
+EOGPCH
+
+chmod +x grit-pre-commit-hook
+
+cat >> pre-commit << 'EOPCH'
+
+## Addes grit as precommit hook
+bash ./.git/hooks/grit-pre-commit-hook
+
+EOPCH
+
 chmod +x pre-commit
+
 echo "Congrats, your project is setup with grit."
 echo "Specify what is a green commit for your project in grit.sh file."
 echo "Happy commits"
+
