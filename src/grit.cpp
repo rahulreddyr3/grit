75fb9f4334bf918c7765a1f243a321ce59f6d473
Rahul <rahulreddyr3@gmail.com>
2019-12-01 13:12:01 +0530

    [Rahul] fix the scripts location such that they can be found from brew installation

--- src/grit.cpp
+++ src/grit.cpp
@@ -10,10 +10,8 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    char* path = argv[0];
-    string folder_name = dirname(dirname(path));
-    string init_script_path = folder_name + "/src/scripts/initialise.sh";
-    string remove_script_path = folder_name + "/src/scripts/remove.sh";
+    string init_script_path = "/usr/local/Cellar/grit/*/src/scripts/initialise.sh";
+    string remove_script_path = "/usr/local/Cellar/grit/*/src/scripts/remove.sh";
 
     if(strcmp(argv[1], "init") == 0) {
         system(init_script_path.c_str());
